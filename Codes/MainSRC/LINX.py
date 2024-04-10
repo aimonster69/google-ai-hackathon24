b0b541a17f6743786dc2c674b5cb92835e580859
akshay kotish <akshaykotish@gmail.com>
2024-04-11 05:06:07 +0530

    functionalities added

--- Codes/MainSRC/LINX.py
+++ Codes/MainSRC/LINX.py
@@ -1,26 +1,21 @@
+import sys
+sys.path.insert(0, '../')
+
 from Factory.Factory import Factory
+from APIs.Jobs import APIs
 
 
 class LINX:
 
     def __init__(self):
         print("Welcome to LINX...")
-        self.factory = Factory()
-        self.Jobs = []
-
-    def PushJob(self):
-        self.Jobs.append(self.factory.Job("Job1"))
-
-    
-
-    def RunJob(self):
-        for job in self.Jobs:
-            job.Run()
-
+        self.factory = Factory("ABC")
+        self.APIs = APIs()
 
     def Begin(self):
-        job1 = self.factory.Job("Job1")
-        job1.Add("", "")
+        analyzer = self.factory.Analyzer()
+        analyzer.start()
+        self.APIs.RunJobsAdd()
 
 
 
